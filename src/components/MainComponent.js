40665df4dec24179e720254518351181c2fa790b
B180248ME <bijjakavya1999@gmail.com>
2022-12-17 23:46:46 +0530

    Single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,15 +5,24 @@ import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders : LEADERS,
+      promotions : PROMOTIONS,
       selectedDish: null
     };
   }
+  
   onDishSelect(dishId){
     this.setState(
         {
@@ -22,9 +31,20 @@ class Main extends Component {
     )
   }
   render(){
+    const DishWithId=({match})=>{
+      return(
+        <DishDetail 
+        dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+        comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}
+        />
+      );
+    };
     const HomePage = () => {
       return(
           <Home 
+          dish={this.state.dishes.filter((dish)=>(dish.featured))[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -34,6 +54,8 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage}/>
           <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/contactus' component={Contact}/>
           <Redirect to="/home" />
         </Switch>
         <Footer/>
